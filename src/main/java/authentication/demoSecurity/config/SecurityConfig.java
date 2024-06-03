8b81d0e6b73ed6bee9404f74eea9d355e5cb8ebc
Luis David <david12.29@hotmail.com>
2024-06-03 11:03:39 -0500

    Refactor, SecurityConfig class is finished

---
+++ src/main/java/authentication/demoSecurity/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package authentication.demoSecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+         return http
+            .csrf(csrf -> 
+                csrf
+                .disable())
+            .authorizeHttpRequests(authRequest ->
+              authRequest
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+                )
+            .formLogin(withDefaults())
+            .build();
+    }
+
+}
