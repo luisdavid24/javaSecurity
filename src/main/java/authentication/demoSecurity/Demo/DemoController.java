ed3cd519fae801d04a794894d19aea2da78b5f21
Luis David <david12.29@hotmail.com>
2024-06-03 10:44:44 -0500

    Refactor, DemoController has some changed about welcom method

--- src/main/java/authentication/demoSecurity/Demo/DemoController.java
+++ src/main/java/authentication/demoSecurity/Demo/DemoController.java
@@ -1,5 +1,18 @@
 package authentication.demoSecurity.Demo;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
 public class DemoController {
 
+    @PostMapping(value="demo")
+    public String welcom(){
+        return "Welcom form security endPoint";
+    }
 }
