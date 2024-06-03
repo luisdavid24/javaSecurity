c8a22d544d7973da30872f8949557ea26334349c
Luis David <david12.29@hotmail.com>
2024-06-03 10:33:03 -0500

    Refactor, AuthController class has some change about login method

--- src/main/java/authentication/demoSecurity/Auth/AuthController.java
+++ src/main/java/authentication/demoSecurity/Auth/AuthController.java
@@ -1,5 +1,19 @@
 package authentication.demoSecurity.Auth;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
+    @PostMapping(value="login")
+    public String login(){
+        return "Login from public endpoint";
+    }
 }
