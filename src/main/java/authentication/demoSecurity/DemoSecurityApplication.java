a085330d5c0319485dc260c0db0fec202d9fc967
Luis David <david12.29@hotmail.com>
2024-06-03 10:09:34 -0500

    Create, this project was created by David

---
+++ src/main/java/authentication/demoSecurity/DemoSecurityApplication.java
@@ -0,0 +1,13 @@
+package authentication.demoSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecurityApplication.class, args);
+	}
+
+}
