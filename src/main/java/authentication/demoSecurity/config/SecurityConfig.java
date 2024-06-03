73d99501633a6e16652a6a5d83f1d364a9b2463c
Luis David <david12.29@hotmail.com>
2024-06-03 11:05:20 -0500

    Refactor, SecurityConfig class is perfect

--- src/main/java/authentication/demoSecurity/config/SecurityConfig.java
+++ src/main/java/authentication/demoSecurity/config/SecurityConfig.java
@@ -2,12 +2,10 @@ package authentication.demoSecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.CsrfToken;
 
 import lombok.RequiredArgsConstructor;
 
