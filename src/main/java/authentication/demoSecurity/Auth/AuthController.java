d39b78c18dae1301a9fcaecdc7ab28f6588c4201
Luis David <david12.29@hotmail.com>
2024-06-05 11:52:58 -0500

    Refactor, I changed some method in AuthController class

--- src/main/java/authentication/demoSecurity/Auth/AuthController.java
+++ src/main/java/authentication/demoSecurity/Auth/AuthController.java
@@ -1,6 +1,8 @@
 package authentication.demoSecurity.Auth;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,13 +14,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 
+      
+
     @PostMapping(value="login")
-    public String login(){
-        return "Login from public endpoint";
+    public ResponseEntity<AuthoReponse> login(@RequestBody LoginRequest request){
+        return ResponseEntity.ok(new AuthoReponse());
     }
 
     @PostMapping(value="register")
-    public String register(){
-        return "Register from public endpoint";
+    public ResponseEntity<AuthoReponse> register(@RequestBody LoginRequest request){
+        return ResponseEntity.ok(new AuthoReponse());
     }
 }
