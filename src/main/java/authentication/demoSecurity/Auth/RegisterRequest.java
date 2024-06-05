3990717f31c6361e54ae597f3941ef5e15490b2e
Luis David <david12.29@hotmail.com>
2024-06-05 11:10:00 -0500

    Refactor, RegisterRequest has some change

--- src/main/java/authentication/demoSecurity/Auth/RegisterRequest.java
+++ src/main/java/authentication/demoSecurity/Auth/RegisterRequest.java
@@ -1,5 +1,9 @@
 package authentication.demoSecurity.Auth;
 
 public class RegisterRequest {
-
+    String username;
+    String password;
+    String firtname;
+    String lastname;
+    String country;
 }
