799a891f6ea42398d0888676c63a03d48db6a8d4
Luis David <david12.29@hotmail.com>
2024-06-05 11:11:28 -0500

    Refactor, RegisterRequest is perfect

--- src/main/java/authentication/demoSecurity/Auth/RegisterRequest.java
+++ src/main/java/authentication/demoSecurity/Auth/RegisterRequest.java
@@ -1,5 +1,14 @@
 package authentication.demoSecurity.Auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterRequest {
     String username;
     String password;
