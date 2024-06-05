aa402994d6353360aa0727f89dea196a07afffe6
Luis David <david12.29@hotmail.com>
2024-06-05 12:05:05 -0500

    Refactor, AuthService class has some annotation

--- src/main/java/authentication/demoSecurity/Auth/AuthService.java
+++ src/main/java/authentication/demoSecurity/Auth/AuthService.java
@@ -1,11 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package authentication.demoSecurity.Auth;
 
+import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 class AuthService {
 
     public AuthoReponse login(LoginRequest request) {
